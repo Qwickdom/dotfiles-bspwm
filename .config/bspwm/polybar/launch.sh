001fc29ab5c308632a137e89c12ff1070a22dc46
PyLess <leonardo.117.265@gmail.com>
2022-04-24 16:31:18 -0500

    .

--- .config/bspwm/polybar/launch.sh
+++ .config/bspwm/polybar/launch.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar bar -c $HOME/.config/bspwm/polybar/config &
-polybar bar2 -c $HOME/.config/bspwm/polybar/config &
+polybar bar -c $HOME/.config/bspwm/polybar/config.ini &
--- .config/bspwm/polybar/scripts/updates.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-yay -Qu | wc -l
